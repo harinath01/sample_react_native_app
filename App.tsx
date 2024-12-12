aa893eb3b7103045b55e162dabb51fed2cf0f31a
hari-testpress <hari@testpress.in>
2024-12-12 11:13:21 +0530

    Update sample code
    Use valid MPD and DRM license URL. Removed unwanted code

--- App.tsx
+++ App.tsx
@@ -1,50 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React, { useEffect, useState } from 'react';
-import {
-  View,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
-
-import Video, { DRMType, } from 'react-native-video';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Video, { DRMType } from 'react-native-video';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [uri, setUri] = useState('')
-
-  useEffect(() => {
-    setTimeout(() => {
-      setUri("https://d1uyp6xlgityir.cloudfront.net/institute/support/courses/drm-videos/videos/transcoded/f98d47fd09b942d7aafc25f7a150a000/video.mpd")
-    }, 500)
-  }, [])
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <View>
       <Video
         source={{
           type: "mpd",
-          uri: 'https://d1uyp6xlgityir.cloudfront.net/institute/support/courses/drm-videos/videos/transcoded/f98d47fd09b942d7aafc25f7a150a000/video.mpd'
+          uri: 'https://d384padtbeqfgy.cloudfront.net/transcoded/AgAFNEJn3kt/video.mpd'
         }}
         drm={{
           type: DRMType.WIDEVINE,
-          licenseServer: "https://support.testpress.in/api/v2.5/chapter_contents/70/drm_license/?access_token=73a7af36-91ad-4736-96b3-f718ca253f85"
+          licenseServer: "https://app.tpstreams.com/api/v1/6eafqn/assets/AgAFNEJn3kt/drm_license/?access_token=f9b11692-78c5-4d14-9385-5f1efb0b8f4e&drm_type=widevine"
         }}
         style={styles.videoPlayer}
         resizeMode="contain"
@@ -57,11 +25,6 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#000',
-  },
   videoPlayer: {
     backgroundColor: "black",
     width: '100%',
