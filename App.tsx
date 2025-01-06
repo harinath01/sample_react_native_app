467f5d52ff1f5f15de9b0c10e7740907ca92e110
hari-testpress <hari@testpress.in>
2025-01-06 10:59:37 +0530

    Add support for fairplay-encrypted media

--- App.tsx
+++ App.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import Video, { DRMType } from 'react-native-video';
+import axios from 'axios';
+
+const CERT_URL = 'https://static.testpress.in/static/fairplay.cer';
 
 function App(): React.JSX.Element {
+  const getDrmConfig = () => {
+    if (Platform.OS === 'ios') {
+      return {
+        type: DRMType.FAIRPLAY,
+        licenseServer: 'https://app.tpstreams.com/api/v1/6eafqn/assets/AgAFNEJn3kt/drm_license/?access_token=f9b11692-78c5-4d14-9385-5f1efb0b8f4e&drm_type=fairplay',
+        certificateUrl: CERT_URL,
+        getLicense: (spcString: string, contentId: string, licenseUrl: string) => {
+          return axios
+            .post(
+              licenseUrl,
+              JSON.stringify({ spc: spcString, assetId: contentId }),
+              {
+                headers: { 'Content-type': 'application/json' },
+                responseType: 'arraybuffer',
+              },
+            )
+            .then(res => {
+              return Buffer.from(res.data, 'binary').toString('base64');
+            })
+            .catch(err => {
+              console.error('DRM ERROR', err);
+            });
+        },
+      };
+    }
+    
+    return {
+      type: DRMType.WIDEVINE,
+      licenseServer: "https://app.tpstreams.com/api/v1/6eafqn/assets/AgAFNEJn3kt/drm_license/?access_token=f9b11692-78c5-4d14-9385-5f1efb0b8f4e&drm_type=widevine"
+    };
+  };
+
   return (
     <View>
       <Video
@@ -10,15 +45,12 @@ function App(): React.JSX.Element {
           type: "mpd",
           uri: 'https://d384padtbeqfgy.cloudfront.net/transcoded/AgAFNEJn3kt/video.mpd'
         }}
-        drm={{
-          type: DRMType.WIDEVINE,
-          licenseServer: "https://app.tpstreams.com/api/v1/6eafqn/assets/AgAFNEJn3kt/drm_license/?access_token=f9b11692-78c5-4d14-9385-5f1efb0b8f4e&drm_type=widevine"
-        }}
+        drm={getDrmConfig()}
         style={styles.videoPlayer}
         resizeMode="contain"
         paused={false}
         controls={true}
-        onError={console.log}
+        onError={(err) => console.error('Player onError', err)}
       />
     </View>
   );
